6272d120d3cd106b7351f99993fbe699d8bc67b5
samSuffy <yobrambilla@gmail.com>
2023-03-10 14:32:11 +0100

    finish logout

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,15 +3,7 @@ import { Link } from "react-router-dom";
 export function LandingPage() {
     return (
         <div>
-            <h1>Bienvenue sur Pep's Design</h1>
-            <h5>Routes disponibles</h5>
-            <div>
-                <Link to="/">Landing page</Link>
-                <Link to="/accueil">Accueil</Link>
-                <Link to="/series">Séries</Link>
-                <Link to="/login">Connexion</Link>
-                <Link to="/register">Inscription</Link>
-            </div>
+            <Link to="/accueil" className="btn btn-primary">Accueil</Link>
         </div>
     );
 }
--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,24 +1,27 @@
-import {  useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export function Logout() {
-;
     const navigate = useNavigate();
+    const [Loading , setLoading] = useState(true);
 
     useEffect(() => {
-        try {
-            axios.post(
-                "https://127.0.0.1:8000/api/logout",
-            );
-            localStorage.removeItem("token");
-            navigate("/accueil");
-        } catch (error) {
-            console.log(error);
-        }
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+        return () => clearTimeout(timer);
     }, []);
 
+    useEffect(() => {
+        if (!Loading) {
+            localStorage.removeItem('token');
+            navigate('/accueil');
+        }
+    }
+    , [Loading, navigate]);
     return (
-        <></>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-primary">Déconnexion en cours...</h1>
+        </div>
     );
 }
